ce20bacc31368c76875cf132c262a244fb24f050
Valerij Zakharenko <konvpalto87@gmail.com>
2026-07-24 16:16:30 +0200

    Add csharp.TUnit version

---
+++ csharp.TUnit/GildedRoseTests/ApprovalTest.cs
@@ -0,0 +1,26 @@
+using GildedRoseKata;
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+using VerifyTUnit;
+
+namespace GildedRoseTests;
+
+public class ApprovalTest
+{
+    [Test]
+    public Task ThirtyDays()
+    {
+        var fakeOutput = new StringBuilder();
+        Console.SetOut(new StringWriter(fakeOutput));
+        Console.SetIn(new StringReader($"a{Environment.NewLine}"));
+
+        Program.Main(new string[] { "30" });
+        var output = fakeOutput.ToString();
+
+        return Verifier.Verify(output);
+    }
+}
---
+++ csharp.TUnit/GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GildedRoseKata;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Test]
+    public async Task Foo()
+    {
+        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        await Assert.That(items[0].Name).EqualTo("fixme");
+    }
+}
